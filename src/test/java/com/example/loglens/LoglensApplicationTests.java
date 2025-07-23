70da57b0f4f44545ec1b57400dacd714e167b4ee
NoteAnywhere <noteanywhere@example.com>
2025-07-23 21:38:18 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/example/loglens/LoglensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loglens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoglensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
